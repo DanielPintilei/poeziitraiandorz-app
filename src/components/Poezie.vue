23387661e47ac65a3295ea80f482523e8a22b358
Daniel Pintilei <danielpintilei@outlook.com>
2017-04-06 22:29:54 +0300

    Disable vertical pan

--- src/components/Poezie.vue
+++ src/components/Poezie.vue
@@ -3,6 +3,7 @@
     class="poezie">
     <transition :name="poezieTransitionName" mode="out-in">
       <v-touch
+        :enabled="{ panup: false, pandown: false }"
         tag="article"
         @swipeleft="nextPoezie"
         @swiperight="prevPoezie"

121f18664ab3a4e3a8c4e772539da3c16ff53612
Daniel Pintilei <danielpintilei@outlook.com>
2017-04-15 20:44:50 +0300

    Remove more first item

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,8 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
+var dbConfig = require('./db.config')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  DATABASE_URL: dbConfig.URL
 })
--- src/components/More.vue
+++ src/components/More.vue
@@ -7,15 +7,6 @@
         <svg
           :fill="theme.icon2"
           height="24" viewBox="0 0 24 24" width="24">
-          <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
-          <path d="M0 0h24v24H0z" fill="none"/>
-        </svg>
-        <span>by Ion Lupiţa</span>
-      </div>
-      <div class="more__item">
-        <svg
-          :fill="theme.icon2"
-          height="24" viewBox="0 0 24 24" width="24">
           <path d="M0 0h24v24H0V0z" fill="none"/>
           <path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/>
         </svg>
